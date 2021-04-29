cf29a32a0ee2f95ddec405206f49b03985230e32
venakata naveen chandra <68272543+naveenchandra143@users.noreply.github.com>
2021-04-29 18:57:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function submit(){
+    var a = prompt("enter first number")
+    var b = prompt("enter second number")
+    function u(a,b){
+        let temp =a;
+    a=b;
+    b=temp;
+    }
+    u(a,b)
+    alert("Check out your answer on console")
+    console.log("after call by value")
+    console.log(a,b)
+}
+function Submit(){
+    var obj = {
+        a: prompt("enter first number"),
+        b: prompt("enter second number")
+    }
+    function u(obj){
+        let temp = obj.a;
+        obj.a = obj.b;
+        obj.b = temp;
+    }
+    u(obj)
+    alert("check out your answer on console")
+    console.log("afer the call by value")
+    console.log(obj)
+}
